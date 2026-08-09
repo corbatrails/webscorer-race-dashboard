2cfc5650b25346b352a8953ab3fbb41b11301ce8
Nathan McCorkle <njmccorkle@gmail.com>
2026-08-08 19:42:56 -0500

    Revert "feat: show gender in category page header"
    This reverts commit 88d28513317003a499bb65aab32b3e65d9bc38af.

--- data_processing.py
+++ data_processing.py
@@ -21,10 +21,8 @@ def process_race_data(api_response):
         name = grouping.get("Category") or grouping.get("Distance") or grouping.get("Gender") or "Overall"
 
         total_finished += len(racers)
-        gender = racers[0].get("Gender", "") if racers else ""
         categories.append({
             "name": name,
-            "gender": gender,
             "racers": racers,
             "leaders": racers[:3],
         })
@@ -47,7 +45,6 @@ def build_pages(dashboard_data, max_rows=18):
         pages.append({
             "type": "category",
             "title": category["name"],
-            "gender": category.get("gender", ""),
             "racers": category["racers"],
         })
 
